eef985cbbdd9fe1428e56c63e908e25cd0f97d6e
Enzo Gabriel <eggqsaraiva@gmail.com>
2021-04-22 20:08:34 -0300

    Adicionando o início do projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,32 @@
+import { GetStaticProps } from 'next';
+
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+}
+
+type HomeProps = {
+  episodes: Episode[];
+}
+
+export default function Home(props: HomeProps) {
+  return (
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch('http://localhost:3333/episodes?_limit=12&_sort=published_at&_order=desc')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 6,
+  }
+}
